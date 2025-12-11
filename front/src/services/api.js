0db02f2ecadc736612d7ded77c1f2838263e7837
Sebastian Calderon <sebastian.1504abril@gmail.com>
2025-12-11 10:18:15 -0500

    Final

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://pricewatch-api-1075919751225.southamerica-east1.run.app",
   headers: {
     "Content-Type": "application/json",
   },
